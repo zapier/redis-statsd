da92779e3c36c380bb8434fdbad9a2437e3b3a85
Rob Golding <rob@robgolding.com>
2015-05-29 12:38:11 +0100

    Fix crash when not running in VERBOSE mode

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ if VERBOSE:
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('[%(asctime)s %(name)s %(levelname)s] %(message)s')
     handler.setFormatter(formatter)
-
-logger.addHandler(handler)
+    logger.addHandler(handler)
 
 
 GAUGES = {
